62b353b1b438cf8eae09f5d0bad6e5ec895f2bb7
jefferson otoni <jeff.otoni@s3wf.com.br>
2020-12-30 22:26:46 -0300

    update large file

--- go1.16/large.file/main.go
+++ go1.16/large.file/main.go
@@ -1,10 +1,34 @@
 package main
 
-import "embed"
+import (
+	"bufio"
+	"embed"
+	"log"
+	"time"
+)
 
 func main() {
+	start := time.Now()
+
 	//go:embed funcionario.json
 	var f embed.FS
-	data, _ := f.ReadFile("funcionario.json")
-	print(string(data))
+
+	// exemplo usando ReadFile
+	// data, _ := f.ReadFile("funcionario.json")
+	// print(string(data))
+
+	file, err := f.Open("funcionario.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		println(scanner.Text())
+	}
+
+	end := time.Now()
+	println("time:", end.Sub(start).String())
 }
